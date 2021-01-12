43b71c31d60ccad621a6bda542e55f00081162c0
Ronan-Fernandes <ronanf.sk@gmail.com>
2021-01-12 18:26:13 -0300

    clientOrdersCards, 13 erros

--- front-end/src/components/ClientOrdersCards.jsx
+++ front-end/src/components/ClientOrdersCards.jsx
@@ -32,45 +32,47 @@ const ClientOrdersCards = () => {
   let dateLength = zero;
 
   return (
-    <>
+    <div>
       {
-        getOrderSuccess && orders.map((order, i) => (
-          dateLength = order.sale_date.length,
-          newDate = new Date(order.sale_date.slice(zero, dateLength - ten)),
-          day = ((`0${newDate.getDate()}`).slice(twoLess)), // Add 0 if day <10
-          month = ((`0${newDate.getMonth()}${1}`).slice(twoLess)), // Add 0 if month <10
-          dateAndMonth = `${day}/${month}`,
-          totalPrice = order.total_price.toFixed(two).toString().replace('.', ','),
-          (<Link
-            key={ order.id }
-            to={ {
-              pathname: `orders/${order.id}`,
-              state: { // passing props para o child
-                date: dateAndMonth,
-                totalPrice,
-              },
-            } }
-          >
-            <div className="cardContainer" data-testid={ `${i}-order-card-container` }>
-              <h3 data-testid={ `${i}-order-number` }>
-                Pedido
-                {order.id}
-              </h3>
-              <h3>{order.status}</h3>
-              <h3 data-testid={ `${i}-order-date` }>
-                {
-                dateAndMonth
-              }
-              </h3>
-              <h3 data-testid={ `${i}-order-total-value` }>
-                R$
-                {totalPrice}
-              </h3>
-            </div>
-          </Link>)
-        ))
+        getOrderSuccess && orders.map((order, i) => {
+          dateLength = order.sale_date.length;
+          newDate = new Date(order.sale_date.slice(zero, dateLength - ten));
+          day = ((`0${newDate.getDate()}`).slice(twoLess)); // Add 0 if day <10
+          month = ((`0${newDate.getMonth()}${1}`).slice(twoLess)); // Add 0 if month <10
+          dateAndMonth = `${day}/${month}`;
+          totalPrice = order.total_price.toFixed(two).toString().replace('.', ',');
+          return (
+            <Link
+              key={ order.id }
+              to={ {
+                pathname: `orders/${order.id}`,
+                state: { // passing props para o child
+                  date: dateAndMonth,
+                  totalPrice,
+                },
+              } }
+            >
+              <div className="cardContainer" data-testid={ `${i}-order-card-container` }>
+                <h3 data-testid={ `${i}-order-number` }>
+                  Pedido
+                  {order.id}
+                </h3>
+                <h3>{order.status}</h3>
+                <h3 data-testid={ `${i}-order-date` }>
+                  {
+                  dateAndMonth
+                }
+                </h3>
+                <h3 data-testid={ `${i}-order-total-value` }>
+                  R$
+                  {totalPrice}
+                </h3>
+              </div>
+            </Link>
+          );
+        })
       }
-    </>
+    </div>
   );
 };
 
